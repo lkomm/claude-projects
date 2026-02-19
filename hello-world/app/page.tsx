a199e925350d57048903030c99b6f39e853c988f
Laxma Kommareddy <laxma@macbookpro.lan>
2026-02-19 12:04:34 -0500

    Initial commit

---
+++ hello-world/app/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white dark:bg-black">
+      <main className="text-center">
+        <h1 className="text-5xl font-bold text-black dark:text-white">
+          Hello, World!
+        </h1>
+        <p className="mt-4 text-lg text-zinc-500">
+          Welcome to my Next.js app
+        </p>
+      </main>
+    </div>
+  );
+}
